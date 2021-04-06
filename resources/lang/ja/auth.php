9b13631b018002fe83a5aac26fda60f8f953c3a7
tomomi66 <nishioka@zero-6.net>
2021-04-06 14:53:12 +0900

    日本語化2

--- resources/lang/ja/auth.php
+++ resources/lang/ja/auth.php
@@ -1,19 +1,7 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    'failed' => 'IDかパスワードのどちらかが間違えています。',
-    'throttle' => 'ログイン試行回数が上限を超えました。 :seconds 分後に再度お試しください。',
-
+    'failed'   => '認証情報と一致するレコードがありません。',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'ログインの試行回数が多すぎます。:seconds 秒後にお試しください。',
 ];
--- resources/lang/ja/passwords.php
+++ resources/lang/ja/passwords.php
@@ -1,22 +1,9 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-
-    'reset' => 'パスワードをリセットしました。',
-    'sent' => 'パスワードのリセット用リンクをメールで送りました。',
-    'throttled' => '再試行するまでお待ちください。',
-    'token' => 'このパスワードリセットは無効です。',
-    'user' => "このメールアドレスのユーザーは見つかりません。",
-
+    'reset'     => 'パスワードをリセットしました。',
+    'sent'      => 'パスワードリマインダーを送信しました。',
+    'throttled' => '時間を置いて再度お試しください。',
+    'token'     => 'このパスワードリセットトークンは無効です。',
+    'user'      => 'このメールアドレスに一致するユーザーを見つけることが出来ませんでした。',
 ];
