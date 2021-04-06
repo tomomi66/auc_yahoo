3e8ee86bdd7799da7f6ac1175dc114332541f1ac
tomomi66 <nishioka@zero-6.net>
2021-04-06 14:46:28 +0900

    日本語化設定

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードをリセットしました。',
+    'sent' => 'パスワードのリセット用リンクをメールで送りました。',
+    'throttled' => '再試行するまでお待ちください。',
+    'token' => 'このパスワードリセットは無効です。',
+    'user' => "このメールアドレスのユーザーは見つかりません。",
+
+];
